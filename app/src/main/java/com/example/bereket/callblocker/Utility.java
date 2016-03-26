a7a47820b55ac4699e9310188f7de7f51fe404dc
Bereket Tesfamichael <neheworld@gmail.com>
2016-03-26 21:38:05 +0300

    interceptor notification dialog theme is modified

--- app/src/main/java/com/example/bereket/callblocker/Utility.java
+++ app/src/main/java/com/example/bereket/callblocker/Utility.java
@@ -19,12 +19,11 @@ public class Utility {
 
         if(alertDialog != null) alertDialog.dismiss();
 
-        //TODO replace texts with those coming fom xml
-        alertDialog = new AlertDialog.Builder(context).create();
-        alertDialog.setTitle("Incoming call intercepted");
-        alertDialog.setMessage("New call is blocked");
+        alertDialog = new AlertDialog.Builder(context, R.style.CallBlockerDialogStyle).create();
+        alertDialog.setTitle(context.getString(R.string.incoming_call_blocked_string));
+        alertDialog.setMessage(context.getString(R.string.incoming_call_intercepted_title));
 
-        alertDialog.setButton(AlertDialog.BUTTON_POSITIVE, "SEE LOG",
+        alertDialog.setButton(AlertDialog.BUTTON_POSITIVE, context.getString(R.string.see_log_text),
                 new DialogInterface.OnClickListener() {
                     public void onClick(DialogInterface dialog, int which) {
 
@@ -35,7 +34,7 @@ public class Utility {
                     }
                 });
 
-        alertDialog.setButton(AlertDialog.BUTTON_NEUTRAL, "CANCEL",
+        alertDialog.setButton(AlertDialog.BUTTON_NEUTRAL, context.getString(R.string.cancel_see_log_text),
                 new DialogInterface.OnClickListener() {
                     public void onClick(DialogInterface dialog, int which) {
                         dialog.dismiss();
